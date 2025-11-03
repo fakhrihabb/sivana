559bce4bd549e6d4896b747b52865ee55de98550
fakhrihabb <fakhrihabb@gmail.com>
2025-11-03 20:25:12 +0700

    chore: changed routing of user page to /sscasn

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,3 @@
-import Navbar from "@/components/Navbar";
-import Hero from "@/components/Hero";
-import DaftarFormasi from "@/components/DaftarFormasi";
-import Footer from "@/components/Footer";
-
 export default function Home() {
-  return (
-    <div className="min-h-screen bg-white">
-      <Navbar />
-      <Hero />
-      <DaftarFormasi />
-      <Footer />
-    </div>
-  );
+  return null;
 }
