97824f46751fac5be024d5376b330ba6b69fb82d
fakhrihabb <fakhrihabb@gmail.com>
2025-11-11 15:57:06 +0700

    refactor: CPNS to CASN

--- src/components/CobaSivanaModal.js
+++ src/components/CobaSivanaModal.js
@@ -84,7 +84,7 @@ export default function CobaSivanaModal({ isOpen, onClose }) {
                     <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20">
                       <path d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" />
                     </svg>
-                    Pendaftar CPNS
+                    Pendaftar CASN
                   </span>
                 </div>
               </button>
